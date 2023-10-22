1c2f57c3e5bc151340a4896a8230255ba0e6d3a2
JoonPark <sargadil@gmail.com>
2023-10-22 14:02:50 +0900

    error test2

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -3,6 +3,6 @@ import Home from './page';
 
 test('checking the text', () => {
   render(<Home />);
-  const textElement = screen.getByText('아 배고파요');
+  const textElement = screen.getByText('아 배고파요!');
   expect(textElement).toBeInTheDocument();
 });
